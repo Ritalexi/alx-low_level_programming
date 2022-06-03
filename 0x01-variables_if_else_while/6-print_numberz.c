87b40930139a480e2c112afefeee822c3d6118c6
Super User <okoliogochukwu83@gmail.com>
2022-06-03 15:00:35 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		putchar(i);
+		putchar((i % 10) + 0);
 	}
 	putchar('\n');
 	return (0);
