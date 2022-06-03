9eee5dd229645b4987ae1984e18fc8bf8dca5349
Super User <okoliogochukwu83@gmail.com>
2022-06-03 15:22:46 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int no;
+	int i;
+	char ch;
+	char j;
+
+	for (no = 0; no <= 9; no++)
+	{
+		i = no;
+	}
+	for (ch = 'a'; ch <= 'f', ch++)
+	{
+		j = ch;
+	}
+	putchar(ij);
+	putchar('\n');
+	return (0);
+}
