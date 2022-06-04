d0af729fa923c069238cd3c4114ac99abf0db673
Super User <okoliogochukwu83@gmail.com>
2022-06-04 00:30:25 -0700

    update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,14 +13,13 @@ int main(void)
 	{
 		for (j = i + 1; j < 10; j++)
 		{
-				putchar((i % 10) + '0');
-				putchar((j % 10) + '0');
-
-				if (i == 8 && j == 9)
-					continue;
-
-				putchar(',');
-				putchar(' ');
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
