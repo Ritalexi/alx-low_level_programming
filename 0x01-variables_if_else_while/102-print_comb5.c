a6389742f49eb0fdaeff0441ace458c50da362c8
Super User <okoliogochukwu83@gmail.com>
2022-06-06 15:13:20 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,15 +12,13 @@ int main(void)
 	{
 		for (j = 0; j <= 8; j++)
 		{
-			putchar((i % 10) + '0');
-			putchar((j % 10) + '0');
-			if (i == 9 && j == 8)
-				continue;
-			putchar(' ');
 			for (k = 0; k <= 9; k++)
 			{
 				for (l = k + 1; l <= 9; l++)
 				{
+					putchar((i % 10) + '0');
+					putchar((j %10) + '0');
+					putchar(' ');
 					putchar((k % 10) + '0');
 					putchar((l % 10) + '0');
 					if (k == 9 && l == 9)
