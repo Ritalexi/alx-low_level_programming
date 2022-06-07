d4a503c99ab6af78ac76c89d872bd7b6c6f2b4e5
Super User <okoliogochukwu83@gmail.com>
2022-06-07 15:47:26 -0700

    int _islower(int c)

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * Return: 1 or 0
+ */
+int _islower(int c)
+{
+	int r;
+
+	r = _islower(c);
+	if (r == true)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
