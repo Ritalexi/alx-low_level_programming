d126444f308e244610442ba26ed6b7c72b6193d6
Super User <okoliogochukwu83@gmail.com>
2022-06-07 16:28:22 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,11 @@
  */
 int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
