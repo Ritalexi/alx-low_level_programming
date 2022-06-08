fde85caf958e49dd3706aec1d4139baef8b7f1ef
Super User <okoliogochukwu83@gmail.com>
2022-06-07 17:36:43 -0700

    commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Entry point
+ *
+ *  @r: number to be checked
+ *
+ *  Return: Always 0
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else 
+	{
+		retuen (r * -1);
+	}
+}
