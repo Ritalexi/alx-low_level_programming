ee172761b2847c0d869df3abee648e196df209f3
Super User <okoliogochukwu83@gmail.com>
2022-06-08 09:46:51 -0700

    int add(int, int

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ *
+ * @i: number to be used
+ *
+ * @j: number to be used
+ *
+ * Return: Always 0
+ */
+int add(int i, int j)
+{
+	int n;
+
+	n = i + j;
+	return (n);
+}
