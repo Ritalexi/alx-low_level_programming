69cf2e35004a96e5ef0880fee9a1eaed69a3111c
Super User <okoliogochukwu83@gmail.com>
2022-06-09 15:33:21 -0700

    upfate

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - Entry point
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	char result;
+
+	result = (a * b) * '1';
+
+	return (result);
+}
