fd881fa237baeb23f9e61a17aef1afdaacff3f9d
Super User <okoliogochukwu83@gmail.com>
2022-06-08 18:16:11 -0700

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,19 +6,19 @@
 int main(void)
 {
 	int i;
-	long int f[98];
+	unsigned long int f[98];
 
 	f[0] = 1;
 	f[1] = 2;
-	printf("%ld, %ld, ", f[0], f[1]);
+	printf("%lu, %lu, ", f[0], f[1]);
 
 	for (i = 2; i < 98; i++)
 	{
 		f[i] = f[i - 1] + f[i - 2];
 		if (i == 97)
-			printf("%ld\n", f[i]);
+			printf("%lu\n", f[i]);
 		else
-			printf("%ld, ", f[i]);
+			printf("%lu, ", f[i]);
 	}
 	return (0);
 }
