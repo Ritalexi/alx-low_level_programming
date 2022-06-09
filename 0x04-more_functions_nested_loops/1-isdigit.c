065425f58c2a2834230feba99374096984a5823e
Super User <okoliogochukwu83@gmail.com>
2022-06-09 15:19:53 -0700

    update

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isdigit(inct c) - Entry point
+ * @c: input
+ * Return: 0
+ */
+
+int _isdigit(int c)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i == c)
+		{
+			j = 1;
+			break;
+		}
+	}
+
+	return (0);
+}
