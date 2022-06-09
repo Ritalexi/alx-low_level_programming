0315176482c06e23f3db3461ef891a6c9a12969b
Super User <okoliogochukwu83@gmail.com>
2022-06-09 15:53:01 -0700

    updaye"

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,18 @@
 
 /**
  * print_numbers - Entry point
- * Return: void
+ * Return: 0
  */
 
 void print_numbers(void)
 {
 	char i;
 
-	for (i = '0'; i <= '9'; i++)
+	i = '0';
+	while (i <= '9')
 	{
 		_putchar(i);
+		i++;
 	}
 	_putchar('\n');
 }
