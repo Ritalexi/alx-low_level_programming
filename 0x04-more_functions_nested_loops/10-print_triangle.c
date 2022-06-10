f6a896d1d9a05819164032055c3af2326db808c9
Super User <okoliogochukwu83@gmail.com>
2022-06-10 16:32:57 -0700

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_triangle - Entry point
+ * @size: input
+ * Return: Always 0
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			if (j <= size - j)
+			{
+				_putchar(' ');
+			}
+			_putchar(35);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
