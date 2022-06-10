a93040650335204b474b45dd19fce58c7cb71942
Super User <okoliogochukwu83@gmail.com>
2022-06-10 14:59:37 -0700

    update

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,18 +4,20 @@
  */
 void more_numbers(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-		_putchar(j % 10 + '0');
-		if (j > 9)
-		{
-		_putchar(j / 10 + '0');
+			k = j;
+			if (j > 9)
+			{
+				_putchar('1');
+				k  = j % 10;
+			}
+			_putchar('0' + k);
 		}
-		}
-	_putchar('\n');
+		_putchar('\n');
 	}
-}}
+}
