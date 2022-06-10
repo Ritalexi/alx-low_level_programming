ef51b25062528039cc99305a2f227d11c454d7cf
Super User <okoliogochukwu83@gmail.com>
2022-06-10 06:44:18 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,10 +2,9 @@
 
 /**
  * print_most_numbers - a function that prints the numbers,
- * from 0 to 9, followed by a new line.
+ * from 0 to 9, except 2 and 4
  * Return: none
  */
-
 void print_most_numbers(void)
 {
 	int i;
