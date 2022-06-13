e354d0924497bb834b17bc3048c1e2f072a784c1
Super User <okoliogochukwu83@gmail.com>
2022-06-13 15:08:23 -0700

    update

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,9 +12,14 @@ void print_rev(char *s)
 	int no = strlen(s);
 	int i;
 
-	for (i = 0; i <= no; i++)
+	for (i = 0; i >= no + 1; i++)
 	{
-		_putchar(s[no - i]);
+		if (s[i] == '\0')
+			break;
+	}
+	for (i = no; i >= 0; i--)
+	{
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
