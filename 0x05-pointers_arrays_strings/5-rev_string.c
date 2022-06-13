1875e78d071f2e35023e86eb7b8d6fc924a07a82
Super User <okoliogochukwu83@gmail.com>
2022-06-13 15:38:25 -0700

    updte

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - Entry point
+ * @s: pointer
+ */
+
+void rev_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+	}
+	for (i = i - 1; i >= 0; i--)
+	{
+		s[i];
+	}
+}
