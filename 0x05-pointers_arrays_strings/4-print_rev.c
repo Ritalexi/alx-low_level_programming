fc969399af8116af56d1302fcb6aa408b072233d
Super User <okoliogochukwu83@gmail.com>
2022-06-13 14:29:25 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_rev - Entry point
+ * @s: pointer
+ */
+
+void print_rev(char *s)
+{
+	int no = strlen(s);
+	int i;
+
+	for (i = 0; i <= no; i++)
+	{
+		_puthar(s[n - i]);
+	}
+	_putchar('\n');
+}
