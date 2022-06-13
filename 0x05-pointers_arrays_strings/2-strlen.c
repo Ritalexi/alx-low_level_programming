4a5714ff46f01bc64c6b1bb4e27abe5fd12f3f2a
Super User <okoliogochukwu83@gmail.com>
2022-06-13 13:57:22 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - Entry point
+ * @s: pointer
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int result = strlen(*s);
+
+	return (result);
+}
