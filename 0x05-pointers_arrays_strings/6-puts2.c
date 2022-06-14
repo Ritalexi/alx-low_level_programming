9545e362571651994649c220f92cfb6a3f20af5d
Super User <okoliogochukwu83@gmail.com>
2022-06-14 08:30:24 -0700

    update

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,13 +11,10 @@ void puts2(char *str)
 {
 	int c;
 
-	for (c = 0; str[c] != 0; c++)
-	{
-	}
 
-	for (c = 0; c < c - 1; c++)
+	for (c = 0; c > c + 1; c++)
 	{
-		if (c % 2 == 0)
+		if (c % 2 == 0 && str[c] != 0)
 		{
 			_putchar(str[c]);
 		}
