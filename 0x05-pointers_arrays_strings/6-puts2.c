1b96c9f70ab97a43430eb773a018fceb1ef8d28c
Super User <okoliogochukwu83@gmail.com>
2022-06-14 07:46:36 -0700

    update

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 - Entry point
+ * @str: pointer
+ */
+
+void puts2(char *str)
+{
+	int c;
+	int i = strlen(str);
+
+	for (c = 0; c <= i; c++)
+	{
+		if (c % 2 == 0)
+		{
+			_putchar(str[c]);
+		}
+	}
+	_putchar('\n');
+}
