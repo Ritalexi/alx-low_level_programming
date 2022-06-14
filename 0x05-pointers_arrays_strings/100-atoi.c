03f367588c16e9e87ed92f75790a0a20427f6167
Super User <okoliogochukwu83@gmail.com>
2022-06-14 14:28:59 -0700

    update

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * @s: pointer
+ * Return: Always 0
+ */
+
+int _atoi(char *s)
+{
+	atoi(s);
+	return (s);
+}
