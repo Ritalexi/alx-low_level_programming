988ec561f571b51abc1788a8fd8d1eebf2e5f7a8
Super User <okoliogochukwu83@gmail.com>
2022-06-14 08:55:56 -0700

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - Entry point
+ * @str: pointer
+ */
+
+void puts_half(char *str)
+{
+	int c, n;
+	int i = strlen(str);
+
+	if (i % 2 == 0)
+	{
+		for (c = i / 2; c < i; c++)
+		{
+			if (str[c] != 0)
+			{
+				_putchar(str[c]);
+			}
+		}
+		_putchar('\n');
+	}
+
+	else if (i % 2 == 1)
+	{
+		n = (i - 1) / 2;
+
+		for (c = n; c < n; c++)
+		{
+			if (str[c] != 0)
+			{
+				_putchar(str[c]);
+			}
+		}
+		_putchar('\n');
+	}
+}
