a0d72aa9c2c3309c4083bafa4c0bfdc4b2012215
Super User <okoliogochukwu83@gmail.com>
2022-06-14 12:45:39 -0700

    update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * print_array - Entry point
+ * @a: pointer
+ * @n: input
+ */
+
+void print_array(int *a, int n)
+{
+	int i, j;
+
+	j = *a;
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+			continue;
+		printf("j[%d], ", i);
+	}
+	printf("\n");
+}
