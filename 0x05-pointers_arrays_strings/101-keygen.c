ae4cb58304ca2fa736b8757835722b9ccd731883
Super User <okoliogochukwu83@gmail.com>
2022-06-14 15:16:05 -0700

    update

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	srand(time(0));
+
+	printf("%d", rand());
+
+	return 0;
+}
