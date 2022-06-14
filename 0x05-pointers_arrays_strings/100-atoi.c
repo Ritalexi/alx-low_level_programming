96ddc1b0ad6f8af87e832aa83b396f439ac79271
Super User <okoliogochukwu83@gmail.com>
2022-06-14 14:58:12 -0700

    update

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,6 +11,33 @@
 
 int _atoi(char *s)
 {
-	int i = atoi(s);
-	return (i);
+	int i, j;
+	unsigned int res;
+	int sign = 1;
+	char now;
+
+	i = 0;
+	res = 0;
+	while (*(s + i) != '\0')
+	{
+		now = *(s + i);
+		if (now == '-')
+		{
+			sign *= -1;
+		}
+		if (now >= '0' && now <= '9')
+		{
+			j = i;
+			while (*(s + j) > 47 && *(s + j) < 58)
+			{
+				res = (res * 10) + *(s + j) - '0';
+				j++;
+			}
+			break;
+		}
+		i++;
+	}
+	if (sign < 0)
+		res *= sign;
+	return (res);
 }
