fff421f0e9488e73623ddbe2a26484d359c62749
Super User <okoliogochukwu83@gmail.com>
2022-06-14 16:03:10 -0700

    update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -12,41 +12,15 @@ int main(void)
 {
 	int i, j;
 
-	srand((unsigned int)(time(NULL)));
-
-	char num[] = "0123456789";
-	char letter[] = "abcdefghijklmnopqrstuvwxyz";
-	char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	char sym[] = "!@#$^&*?";
-	char password[N];
-
-	j = rand() % 4;
-
-	for (i = 0; i < N; i++)
+	srand(time(NULL));
+	i = 0;
+	while (i <= 2645)
 	{
-		if (j == 1)
-		{
-			password[i] = num[rand() % 10];
-			j = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (j == 2)
-		{
-			password[i] = symbols[rand() % 8];
-			j = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else if (j == 3)
-		{
-			password[i] = LETTER[rand() % 26];
-			j = rand() % 4;
-			printf("%c", password[i]);
-		}
-		else
-		{
-			password[i] = letter[rand() % 26];
-			j = rand() % 4;
-			printf("%c", password[i]);
-		}
+		j = (rand() % 128);
+		i += j;
+		printf("%c", j);
 	}
+	printf("%c", 2772 - i);
+
+	return (0);
 }
