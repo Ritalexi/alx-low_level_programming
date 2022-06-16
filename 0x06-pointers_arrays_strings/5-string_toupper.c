05c4121ca79a0b0cdab9751aedacafdfd356362d
Super User <okoliogochukwu83@gmail.com>
2022-06-16 12:07:38 -0700

    update

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,19 +3,19 @@
 
 /**
  * string_toupper - Entry point
+ * @s: pointer
  * Return: Always 0
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
-	a;
 	int i;
 
-	for (i = 0; [i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if ([i] >= 'a' && [i] <= 'z')
+		if (s[i] >= 'a' && s[i] <= 'z')
 		{
-			[i] = [i] - 32;
+			s[i] = s[i] - 32;
 		}
 	}
 	return (0);
