15a26310b74a6f9a43aa82fffd401905649c2e86
Super User <okoliogochukwu83@gmail.com>
2022-06-16 11:35:07 -0700

    update

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio h>
+#include "main.h"
+
+/**
+ * string_toupper - Entry point
+ * Return: Always 0
+ */
+
+char *string_toupper(char *)
+{
+	a;
+	int i;
+
+	for (i = 0; [i] != '\0'; i++)
+	{
+		if ([i] >= 'a' && [i] <= 'z')
+		{
+			[i] = [i] - 32;
+		}
+	}
+	return (0);
+}
