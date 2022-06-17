78f3eb1626b8e51122b0fe653b4af5c78ed437fe
Super User <okoliogochukwu83@gmail.com>
2022-06-17 14:38:37 -0700

    update

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * rot13 - Entry point
+ * @s: ponter
+ * Return: Always 0
+ */
+
+char *rot13(char *s)
+{
+	int i, j;
+
+char a[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"};
+char b[] = {"NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm"};
+
+for (i = 0; s[i] != '\0'; i++)
+{
+	for (j = 0; j < 52; j++)
+	{
+		if (s[i] == a[j])
+		{
+			s[i] = b[j];
+			continue;
+		}
+	}
+}
+return (s);
+}
