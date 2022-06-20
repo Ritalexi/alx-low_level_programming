84d1e5c0cfc5d6c29fb42a0aae76fe32035ff603
Super User <okoliogochukwu83@gmail.com>
2022-06-20 14:15:52 -0700

    update

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,7 +2,7 @@
 
 /**
  * _strspn - Entry point
-i * @char: pointer
+ * @char: pointer
  * @accept: pointer
  * Return: Always 0
  */
@@ -11,16 +11,16 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i, j, n = 0;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; *s; i++)
 	{
 		for (j = 0; accept[j]; j++)
 		{
 			if (s[i] == accept[j])
 			{
 				n++;
-				continue;
+				break;
 			}
-			else if (accept[i + 1] == '\0')
+			else if (accept[j + 1] == '\0')
 				return (n);
 		}
 	}
