ee0d6dee9073dda51afe71e29268e0909eb288a2
Ritalexia <70811710+Ritalexi@users.noreply.github.com>
2022-06-21 12:01:19 +0100

    Update 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,27 +1,23 @@
 #include "main.h"
 
 /**
- * _strpbrk - Entry point
- * @s: pointer
- * @accept: pointer
+ * _strpbrk - searches a string for any of a set of strings.
+ * @s: first string.
+ * @accept: second string.
+ * Return: a pointer to the string in s that matches one of the
+ * or NULL
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
-	char *k = accept;
+	unsigned int i, j;
 
-	for (i = 0; *s; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; *accept; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (*s == *accept)
-			{
-				return (s);
-			}
-			accept++;
+			if (s[i] == accept[j])
+				return (s + i);
 		}
-			accept = k;
-			s++;
 	}
+	return ('\0');
 }
