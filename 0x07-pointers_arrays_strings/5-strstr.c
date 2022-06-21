65366561926d360f8220d188341db7eb4e36b057
Ritalexia <70811710+Ritalexi@users.noreply.github.com>
2022-06-21 12:21:51 +0100

    Create 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,16 +1,30 @@
 #include "main.h"
-#include <string.h>
+#include <stdio.h>
 
 /**
- * _strstr - Entry point
- * @haystack: pointer
- * @needle: pointer
- * Return: Always 0
+ * _strstr -  a function that locates a substring.
+ * @haystack: an input string to search in
+ * @needle: an input string to locate into string haystack
+ * Return:  a pointer to the beginning of the located substring,
+ * or NULL if the substring is not found.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	strstr(haystack, needle);
+	char *startn = needle, *starth = haystack;
 
-	return (0);
+	while (*haystack)
+	{
+		starth = haystack;
+		needle = startn;
+		while (*haystack == *needle)
+		{
+			haystack++;
+			needle++;
+		}
+
+		if (*needle == '\0')
+			return (haystack);
+		haystack = starth + 1;
+	}
+	return (NULL);
 }
