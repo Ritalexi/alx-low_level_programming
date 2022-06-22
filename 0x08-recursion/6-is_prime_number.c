bb1577f7f8477ece6a0df0e1164ad9fdd251f451
Super User <okoliogochukwu83@gmail.com>
2022-06-22 16:05:11 -0700

    update

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+int prime(int p, int r);
+
+/**
+ * is_prime_number - Entry point
+ * @n: input
+ * Return: Always 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+
+	else
+	}
+return (prime(2, n));
+}
+}
+
+/**
+ * prime - Wntry point
+ * @p: input
+ * @r: input
+ * Return: Always 0
+ */
+
+int prime(int p, int r)
+{
+	if ((r / 2) % p != 0)
+	{
+		return (1);
+	}
+	else 
+		return (0);
+
+	return (prime(p + 1, r));
+}
