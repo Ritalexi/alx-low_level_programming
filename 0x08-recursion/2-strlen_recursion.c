c185298dfaccfaab36f304d4cec9396358420bdf
Super User <okoliogochukwu83@gmail.com>
2022-06-22 12:59:23 -0700

    update

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,12 +10,12 @@ int _strlen_recursion(char *s)
 {
 	int i = 0;
 
-	if (*s == '\0')
-		return;
-
-	i++;
-	_strlen_recursion(s + 1);
-	_putchar(i);
+	if (*s != '\0')
+	{
+		i++;
+		_strlen_recursion(s + 1);
+		_putchar(i);
+	}
 
 	return (0);
 }
