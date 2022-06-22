a9a714d14e2248c375f0aa51e4fb3924b96711ce
Super User <okoliogochukwu83@gmail.com>
2022-06-22 15:11:15 -0700

    update

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion -Entry point
+ * @n: parameter
+ * Return: Always 0
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return(_sqrt(1, n));
+}
+
+/**
+ * _sqrt - Entry point
+ * @p: previou value
+ * @r: root value
+ * Retun: Always 0
+ */
+
+int _sqrt(int p, int r)
+{
+	if (p > r)
+		return (-1);
+
+	else if (p * p == r)
+	{
+		return (p);
+	}
+	
+	return (_sqrt(p + 1, r));
+}
