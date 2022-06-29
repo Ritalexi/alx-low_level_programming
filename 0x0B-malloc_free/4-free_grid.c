c56a95c7ac1192b6354b17f09801fc437973b566
Super User <okoliogochukwu83@gmail.com>
2022-06-29 14:30:37 -0700

    update

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * free_grid - Entry point
+ * @grid: pointer of pointer
+ * @height: input
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != 0)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+
+		free(grid);
+	}
+}
