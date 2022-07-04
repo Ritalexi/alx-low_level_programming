2963194f593f69fef839728538d3ac58bd328ced
Super User <okoliogochukwu83@gmail.com>
2022-07-04 13:26:19 -0700

    update

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,17 +1,6 @@
 #ifndef __function_like_macro__
 #define __function_like_macro__
 
-#define ABS(x) x
-
-/**
- * main - Entry point
- */
-
-int main()
-{
-#if x < 0
-	(x * -1);
-#endif
-}
+#define ABS(x) ((x < 0) ? (x * -1) : (x))
 
 #endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef __sum__
+#define __sum__
+
+#define SUM(x, y) ((x) + (y))
+
+#endif
