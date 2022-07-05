41da01b13a99dbf38cd7c2d7d0c8d3f5d4abf23f
Super User <okoliogochukwu83@gmail.com>
2022-07-05 11:46:06 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,27 @@
-struct dog
+#ifndef _dog_h_
+#define _dog_h_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct main - Entry point
+ * @name: input
+ * @age: input
+ * @owner: input
+ * Description: struct called dog to make a mode
+ */
+
+struct main
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
