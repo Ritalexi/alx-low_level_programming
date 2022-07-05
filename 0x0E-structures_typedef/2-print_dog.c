8146739fe61eed5d8fed78de96799aa5be25b8f6
Super User <okoliogochukwu83@gmail.com>
2022-07-05 13:01:51 -0700

    update

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * print_dog - Event point
+ * @d: pointer
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		printf(NULL);
+
+	if (d->name == NULL || d->age == NULL || d->owner == NULL)
+		printf("(nil)");
+
+	printf("Name: %s\nAge: %.1f\nOwner: %s\n", d->name, d->age, d->owner);
+}
