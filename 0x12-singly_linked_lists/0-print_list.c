3d5a3308b73a7f043df1f28cde0e0f2512da8e26
Super User <okoliogochukwu83@gmail.com>
2022-07-14 13:36:10 -0700

    update

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - Entry point
+ * @list_t *h - struct pointer
+ *
+ * Return: Always 0
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		if(h->str != NULL)
+		{
+		printf("[%u] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+
+	return (count);
+}
