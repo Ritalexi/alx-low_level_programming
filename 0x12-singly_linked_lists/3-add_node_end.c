733ac2d435c5751115683533fca14cfea2e25602
Super User <okoliogochukwu83@gmail.com>
2022-07-14 17:10:38 -0700

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,19 +13,31 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *ptr, *end;
 	size_t i;
 
-	ptr = *head;
+	if (!(head && str))
+		return (NULL);
+
 	end = malloc(sizeof(list_t));
-	if (ptr == NULL)
+
+	if (!end)
 		return (NULL);
 
 	end->str = strdup(str);
-
 	for (i = 0; str[i]; i++)
 		;
 	end->len = i;
 
+	if (!*head)
+	{
+		ptr = *head;
+		return (ptr);
+	}
+
+	ptr = *head;
+
 	while (ptr->next != NULL)
+	{
 		ptr = ptr->next;
+	}
 
 	ptr->next = end;
 
