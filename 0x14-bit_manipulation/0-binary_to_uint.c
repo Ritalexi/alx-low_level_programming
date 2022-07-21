74b1efb6987bb6e3ce143a89ef35709829f889cb
Super User <okoliogochukwu83@gmail.com>
2022-07-21 13:42:21 -0700

    update

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * int binary_to_uint - Entry point
+ * @b: pointer
+ * Return: Always 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int ui;
+	int i, j;
+
+	if (b == NULL)
+		return (0);
+
+	ui = 0;
+
+	for (i = 0; b[i] != '\0'; i++)
+	{
+	}
+
+
+	for (i--, j = 1; i >= 0; i--, j *= 2)
+	{
+		if (b[i] & 1)
+		{
+			ui += j;
+		}
+		else if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+
+	return (ui);
+}
