7152ad47c90667a1ba061d8e070dfc938fcf3538
Super User <okoliogochukwu83@gmail.com>
2022-07-22 08:50:28 -0700

    update

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * get_endianness - Entry point
+ * Return: Always 0
+ */
+
+int get_endianness(void)
+{
+	unsigned int x;
+	char *i;
+
+	x = 1;
+	i = (char *) &x;
+	return ((int)*c);
+}
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,4 +6,17 @@
  * @n: number one.@n: number one.
  * @m: number two.
  * Return: number of bits.
- */
+*/
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int nbits;
+
+	for (nbits = 0; n || m; n >>= 1, m >> 1)
+	{
+		if ((n & 1) != (m & 1))
+			nbits++;
+	}
+
+	return (nbits);
+}
