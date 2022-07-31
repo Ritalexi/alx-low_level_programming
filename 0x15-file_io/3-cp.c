8fdab4ea41e3cb33ebab07b29e3d8adaf1d05420
Super User <okoliogochukwu83@gmail.com>
2022-07-31 09:12:39 -0700

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -23,7 +23,8 @@ int main(int argc, char *argv[])
 
 	}
 	file_to = open(argv[2], 
-			O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+			O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | 
+			S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
